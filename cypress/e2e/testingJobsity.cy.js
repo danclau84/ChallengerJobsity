aa3a235b20ed80e8db9022e64c6302510391b397
Daniela Carrillo <daniela.carrillo@outlook.com>
2022-06-13 17:14:15 -0500

    Testing fields validation

--- cypress/e2e/testingJobsity.cy.js
+++ cypress/e2e/testingJobsity.cy.js
@@ -1,8 +1,11 @@
 describe("Testing - AutomationPractice.com", () =>{
-    it("Validate Customer Form",() =>{
+    beforeEach(() => {
         cy.visit("http://automationpractice.com") //visit the page
         cy.get('#contact-link > a').click() // click over link contact
-        cy.get('#id_contact').select("Customer service") //Select option Costumer service
+      })
+    it("Validate customer form",() =>{
+        
+        cy.get('#id_contact').select("Customer service") //Select option Customer service
         cy.get('#email').type("daniela.carrillo@outlook.com") //Input a email
         cy.get('#id_order').type("123456") //Input order number
         cy.fixture('logo.jpg').as('logo') //Set logo fixture
@@ -12,9 +15,8 @@ describe("Testing - AutomationPractice.com", () =>{
         cy.get('.alert').should("have.text","Your message has been successfully sent to our team.") // expect a successful message
     })
 
-    it("Validate Webmaster Form",() =>{
-        cy.visit("http://automationpractice.com") //visit the page
-        cy.get('#contact-link > a').click() // click over link contact
+    it("Validate webmaster form",() =>{
+        
         cy.get('#id_contact').select("Webmaster") //Select option Webmaster service
         cy.get('#email').type("daniela.carrillo@outlook.com") //Input a email
         cy.get('#id_order').type("123456") //Input order number
@@ -25,5 +27,18 @@ describe("Testing - AutomationPractice.com", () =>{
         cy.get('.alert').should("have.text","Your message has been successfully sent to our team.") // expect a successful message
     })
 
+    it("Validate empty form",() =>{
+        cy.get('#submitMessage > span').click() //submit a button send
+        //cy.get('ol > li').should("have.text","There is 1 error") // expect a successful message
+        cy.contains('Invalid email address.')
+        cy.get('#email').type("daniela.carrillo@outlook.com") //Input a email
+        cy.get('#submitMessage > span').click() //submit a button send
+        cy.contains('The message cannot be blank.')
+        cy.get('#message').type("Testing text automation") //imput message
+        cy.get('#submitMessage > span').click() //submit a button send
+        cy.contains('Please select a subject from the list provided.')
+        cy.get('#id_contact').select("Webmaster") //Select option Webmaster service
+        cy.get('#submitMessage > span').click() //submit a button send
+    })
 
 })
